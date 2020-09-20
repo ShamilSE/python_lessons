d2471e22fb22d78578e04c4180273bab8535f753
ShamilSE <54128722+ShamilSE@users.noreply.github.com>
2020-09-20 10:06:47 -1200

    trying to resolve 100 case, some errors

--- codewars/Vasya-Clerk.py
+++ codewars/Vasya-Clerk.py
@@ -9,11 +9,34 @@ def tickets(people):
             while j < len(stack):
                 if stack[j] == 25:
                     del stack[j]
-                    stack.append(people[j])
+                    stack.append(people[i])
+                    j = 0
                     break
-                j = j + 1
+                j += 1
             else:
                 return "NO"
-        i = i + 1
+        if people[i] == 100:
+            bill = 100
+            while j < len(stack):
+                if stack[j] == 50:
+                    del stack[j]
+                    j = 0
+                    bill -= stack[j] 
+                    break
+                j += 1
+            j = 0
+            while j < len(stack) or bill == 25:
+                if bill == 25:
+                    stack.append(people[i])
+                    j = 0
+                    break
+                if stack[j] == 25:
+                    bill -= stack[j]
+                    del stack[j]
+                    j -= 1
+                j += 1
+            else:
+                return "NO"
+                j += 1
+        i += 1
     return "YES"
-
